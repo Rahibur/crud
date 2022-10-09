b4662978078c22fc1a3fb6d2006ed8a41251f43c
R.Rahib <79588712+Rahibur@users.noreply.github.com>
2022-10-09 18:02:41 +0600

    Add files via upload

---
+++ partials/_nav2.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-lg navbar-light"style="background-color: #f8f6f77a;">
+  <a class="navbar-brand" href="/crud/index.php"style=" font-family: cursive ">Todos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
+
